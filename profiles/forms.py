b4673334cc37f14b8d4f1dc5d8f8abf39793927e
Kayla Perez <perezkay@uw.edu>
2024-05-23 15:02:14 -0700

    removed first name requirement

--- profiles/forms.py
+++ profiles/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm, CharField
 from profiles.models import Profile
 
 class ProfileUpdateForm(ModelForm):
-	first_name = CharField(max_length=32, required=True)
+	first_name = CharField(max_length=32, required=False)
 	last_name = CharField(max_length=32, required=False)
 	class Meta:
 		model = Profile
