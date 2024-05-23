f8148690fcd64992a26f44a81b027cae8d723a2f
Kayla Perez <perezkay@uw.edu>
2024-05-23 15:23:11 -0700

    populated first and last name with the current values in the model

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,9 +1,29 @@
-from django.forms import ModelForm, CharField
+from django.forms import ModelForm, CharField, TextInput, Textarea
 from profiles.models import Profile
 
 class ProfileUpdateForm(ModelForm):
-	first_name = CharField(max_length=32, required=False)
-	last_name = CharField(max_length=32, required=False)
+	first_name = CharField(max_length=32, required=False,widget=
+				TextInput(attrs={
+				'class': 'form-control',
+				'style': 'max-width: 300px;'
+			}))
+	last_name = CharField(max_length=32, required=False,widget=
+				TextInput(attrs={
+				'class': 'form-control',
+				'style': 'max-width: 300px;'
+			}))
 	class Meta:
 		model = Profile
 		fields = ['profile_picture', 'bio', 'banner']
+		widgets = {
+			'bio': Textarea(attrs={
+				'class': 'form-control',
+				'rows': '3',
+				'style': 'max-width: 500px;'
+			})
+		}
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['first_name'].initial = self.instance.user.first_name
+		self.fields['last_name'].initial = self.instance.user.last_name
